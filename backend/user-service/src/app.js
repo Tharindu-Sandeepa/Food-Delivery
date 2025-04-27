d05098cf18cc2255ccc4ba649660c9735b12fb8d
Ridma <158838958+Ridma-Sathsara@users.noreply.github.com>
2025-04-27 15:31:58 +0530

    change the user-management backend port to 3005

--- backend/user-service/src/app.js
+++ backend/user-service/src/app.js
@@ -55,7 +55,7 @@ app.use('/api/v1/users', userRoutes);
 // Error handler middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3005;
 
 const server = app.listen(
   PORT,
