267be5ddb6e657e33838cdcec583d5139fec2f5d
lahiruasiri <lahiruasiri1@gmail.com>
2025-04-27 16:01:03 +0530

    Update orderRoutes.js

--- backend/order-service/routes/orderRoutes.js
+++ backend/order-service/routes/orderRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/orderController");
 
-router.patch('/:orderId/ready', controller.markOrderReady)
+router.patch("/:orderId/ready", controller.markOrderReady);
 router.post("/driver-assigned", controller.receiveDriverAssignment);
 router.patch("/delivery-complete", controller.updateOrderStatusByDeliveryId);
 router.post("/", controller.createOrder);
+router.post("/payments", controller.processPayment); // New endpoint for Stripe payments
 router.get("/:id", controller.getOrderById);
 router.get("/restaurant/:id", controller.getOrderByRestaurantId);
-router.get("/", controller.getAllOrders); // optional
+router.get("/", controller.getAllOrders);
 router.patch("/:id/status", controller.updateOrderStatus);
+router.get("/customer/:id", controller.getOrderByCustomerId);
 
 module.exports = router;
